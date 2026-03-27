1738cf3f45e78d4dc835941530265c87d991409d
Ranjula1014 <it24104259@my.sliit.lk>
2026-03-27 19:36:09 +0530

    Heartbeat

--- server1.py
+++ server1.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Server 1 is running"}
+
+@app.get("/heartbeat")
+def heartbeat():
+    return {"status": "alive", "server": "server1"}
--- server2.py
+++ server2.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Server 2 is running"}
+
+@app.get("/heartbeat")
+def heartbeat():
+    return {"status": "alive", "server": "server2"}
