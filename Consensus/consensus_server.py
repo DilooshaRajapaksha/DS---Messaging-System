85ac354f91cb816e9de5907803b6e0d9b4c863d6
Pasindu Ashen <it24610781@my.sliit.lk>
2026-04-04 16:42:51 +0530

    add simple HTML chat interface for testing distributed messaging

--- Consensus/consensus_server.py
+++ Consensus/consensus_server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from Dilanka.raft_node import RaftNode, RaftState
 from Dilanka.models import Message, MessageStore
-from Time_Synch.Time_sync import SynchronizedClock
+from Time_sync import SynchronizedClock
 import asyncio
 import os
 import sys
